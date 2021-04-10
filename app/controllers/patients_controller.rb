95efade879cac772a5a3679b39e01028041c8a35
Dexter <dex429.dc@gmail.com>
2021-04-10 15:31:50 -0400

    add session controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,21 +1,21 @@
 class PatientsController < ApplicationController
 
-    get "/sign_up" do 
-      erb :"/patients/new"
-    end 
+  get "/sign_up" do 
+    erb :"/patients/new"
+  end 
   
-    post "/sign_up" do 
-      if params[:patient][:password] == params[:patient][:password_confirmation] && params[:patient][:email] && params[:patient][:username]
-        @patient = Patient.new(params[:patient])
-        if @patient.save 
-          session[:patient_id] = @patient.id
-          redirect to "/strains"
-        else 
-          redirect to "/sign_up"
-        end
+  post "/sign_up" do 
+    if params[:patient][:password] == params[:patient][:password_confirmation] 
+    params[:patient][:email] && params[:patient][:username]
+      @patient = Patient.new(params[:patient])
+      if @patient.save 
+        session[:patient_id] = @patient.id
+        redirect to "/strains"
       else 
         redirect to "/sign_up"
-      end 
+      end
+    else 
+      redirect to "/sign_up"
     end 
-  
-  end
+  end  
+ end

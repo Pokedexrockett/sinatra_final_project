358eb3f0e9766cd9d05714bdbe34ce94562adb1d
Dexter <dex429.dc@gmail.com>
2021-04-10 15:13:22 -0400

    add sign up post method to patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,18 +4,18 @@ class PatientsController < ApplicationController
       erb :"/patients/new"
     end 
   
-    # post "/sign_up" do 
-    #   if params[:patient][:password] == params[:patient][:password_confirmation] && params[:patient][:email] && params[:patient][:username]
-    #     @patient = Patient.new(params[:patient])
-    #     if @patient.save 
-    #       session[:patient_id] = @patient.id
-    #       redirect to "/strains"
-    #     else 
-    #       redirect to "/sign_up"
-    #     end
-    #   else 
-    #     redirect to "/sign_up"
-    #   end 
-    # end 
+    post "/sign_up" do 
+      if params[:patient][:password] == params[:patient][:password_confirmation] && params[:patient][:email] && params[:patient][:username]
+        @patient = Patient.new(params[:patient])
+        if @patient.save 
+          session[:patient_id] = @patient.id
+          redirect to "/strains"
+        else 
+          redirect to "/sign_up"
+        end
+      else 
+        redirect to "/sign_up"
+      end 
+    end 
   
   end
